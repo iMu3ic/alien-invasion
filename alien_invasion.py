c220cf80c606eb8f2e2394b8ffe67bc8eab2538b
iMu3ic <iMu3ic@github.com>
2022-10-23 08:39:11 +0300

    Added screen filling with bg_color

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,6 +10,8 @@ class AlienInvasion():
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+        #Назначение цвета фона
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Запуск основного цикла игры."""
@@ -19,6 +21,9 @@ class AlienInvasion():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            #При каждом проходе цикла перерисовывается экран.
+            self.screen.fill(self.bg_color)
+            
             #Отображение последнего прорисованного экрана.
             pygame.display.flip()
 
