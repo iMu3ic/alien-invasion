07183978d6c503b6174ee032ae74cdbdf9c5f39c
iMu3ic <iMu3ic@github.com>
2022-11-13 21:28:49 +0300

    Added full screen resolution and Q-escape.

--- settings.py
+++ settings.py
@@ -3,7 +3,10 @@ class Settings():
 
     def __init__(self):
         """Инициализирует настройки игры"""
-        #Параметры экрана
+        # Параметры экрана
         self.screen_width = 1200
         self.screen_high = 800
         self.bg_color = (230, 230, 230)
+        # Настройки корабля
+        self.ship_speed = 1.5
+        
