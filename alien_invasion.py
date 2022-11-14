c62bae763e262748cb165184cc3278ef8c3c989b
iMu3ic <iMu3ic@github.com>
2022-11-14 20:45:41 +0300

    Added shooting.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion():
     """Класс для управления ресурсами и поведением игры."""
@@ -10,20 +11,19 @@ class AlienInvasion():
         """Инициализирует игру и создает игровые ресурсы."""
         pygame.init()
         self.settings = Settings()  
-
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
-
         pygame.display.set_caption("Alien Invasion")
-
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -43,6 +43,8 @@ class AlienInvasion():
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
     
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -50,11 +52,28 @@ class AlienInvasion():
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Создание нового снаряда и включение его в группу bullets."""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Обновляет позиции снарядов и уничтожает старые снаряды."""
+        # Обновление позиций снарядов.
+        self.bullets.update()
+
+        # Удаление снарядов, вышедших за край экрана.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
     def _update_screen(self):
             """Обновляет изображение на экране и отображает новый экран."""
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
-            
+            for bullet in self.bullets.sprites():
+                bullet.draw_bullet()
             pygame.display.flip()  
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -9,4 +9,9 @@ class Settings():
         self.bg_color = (230, 230, 230)
         # Настройки корабля
         self.ship_speed = 1.5
-        
+        # Параметры снаряда
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
